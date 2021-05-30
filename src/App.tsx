f25adf8146432cc551085fd06dee745f3ca9e3a9
henrylsilveira <henrylsilveira@gmail.com>
2021-05-30 13:01:13 -0300

    Finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
+import Modal from 'react-modal';
+import { useState } from 'react';
+import { Header } from "./components/Header";
+import { Dashboard } from "./components/Dashboard";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyle } from "./styles/global";
+import { TransactionsProvider } from './hooks/useTransactions';
 
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+      setIsNewTransactionModalOpen(true);
+  }
+  function handleCloseNewTransactionModal() {
+      setIsNewTransactionModalOpen(false);
+  }
+
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <TransactionsProvider>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <Dashboard />
+      <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal} />
       <GlobalStyle />
-    </div>
+    </TransactionsProvider>
   );
 }
 
